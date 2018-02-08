1320bbd9459499a3f76d5f763398330be92a43f1
Denis Ciccardi <denis.ciccardi@gmail.com>
2018-02-08 23:11:38 +0100

    aggiunti fragment partiti

--- app/src/main/java/dvrdr/articolo48/PiuEuropa.java
+++ app/src/main/java/dvrdr/articolo48/PiuEuropa.java
@@ -4,23 +4,20 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageButton;
 
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link Partiti.OnFragmentInteractionListener} interface
+ * {@link PiuEuropa.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link Partiti#newInstance} factory method to
+ * Use the {@link PiuEuropa#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Partiti extends Fragment {
+public class PiuEuropa extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -32,7 +29,7 @@ public class Partiti extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public Partiti() {
+    public PiuEuropa() {
         // Required empty public constructor
     }
 
@@ -42,11 +39,11 @@ public class Partiti extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Partiti.
+     * @return A new instance of fragment PiuEuropa.
      */
     // TODO: Rename and change types and number of parameters
-    public static Partiti newInstance(String param1, String param2) {
-        Partiti fragment = new Partiti();
+    public static PiuEuropa newInstance(String param1, String param2) {
+        PiuEuropa fragment = new PiuEuropa();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -57,7 +54,7 @@ public class Partiti extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       if (getArguments() != null) {
+        if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
@@ -67,21 +64,7 @@ public class Partiti extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        getActivity().setTitle("Partiti");
-
-        View view = inflater.inflate(R.layout.fragment_partiti, container, false);
-
-        final ImageButton pd = (ImageButton) view.findViewById(R.id.pd);
-        pd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Fragment frag_pd = new Pd();
-
-                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-                fragmentManager.beginTransaction().replace(R.id.flContent, frag_pd).addToBackStack(null) .commit();
-            }
-        });
-        return view;
+        return inflater.inflate(R.layout.fragment_piu_europa, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
